7a988ee258079c12c10053c23dc0d447c537db49
Joe <joeaquiare@gmail.com>
2023-11-14 00:58:17 -0500

    Updated home site.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,15 @@
-import { SitePage } from "../styles/siteStyles";
+import { SitePage, TextBlock } from "../styles/siteStyles";
 
 // Home page element
 function Home() {
   return (
     <SitePage>
-      Hey there, my name is Joe - I'm a professional software engineer who
-      enjoys game development, music production, illustration, and graphic
-      design. For inquiries, or if you'd like to work together, please email me
-      at joeaquiare@gmail.com.
+      <TextBlock>
+        Hey there, my name is Joe - I'm a professional software engineer who enjoys game development, music production, illustration, and graphic design. For inquiries, or if you'd like to work together, please email me at joeaquiare@gmail.com.
+      </TextBlock>
+      <TextBlock>
+        This site is a work in progress.
+      </TextBlock>
     </SitePage>
   );
 }
