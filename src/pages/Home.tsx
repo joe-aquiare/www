3fd6b240059fb09e9ded27c1801f527d7bf28b8f
Joe <joeaquiare@gmail.com>
2023-11-15 00:04:59 -0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { SectionHeader, SitePage, TextBlock } from "../styles/siteStyles";
+import { ExternalLink, SectionHeader, SitePage, TextBlock } from "../styles/siteStyles";
 
 // Home page element
 function Home() {
@@ -6,7 +6,7 @@ function Home() {
     <SitePage>
       <SectionHeader>Home</SectionHeader>
       <TextBlock>
-        Hey there, my name is Joe - I'm a professional software engineer who enjoys game development, music production, illustration, and graphic design. For inquiries, or if you'd like to work together, please email me at joeaquiare@gmail.com.
+        Hey there, my name is Joe - I'm a professional software engineer who enjoys game development, music production, illustration, and graphic design. For inquiries, or if you'd like to work together, please email me at <ExternalLink href="mailto:joeaquiare@gmail.com">joeaquiare@gmail.com</ExternalLink>.
       </TextBlock>
       <TextBlock>
         This site is a work in progress.
