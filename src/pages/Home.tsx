5d05db4c4d564f8722752a779cf24d6ff0e23dbc
Joe <joeaquiare@gmail.com>
2023-11-20 00:34:03 -0500

    Updated user select on home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,13 +9,14 @@ function Home() {
         Hey there, my name is Joe - I'm a professional software engineer who enjoys game development, music production, illustration, and graphic design. For inquiries, or if you'd like to work together, please email me at <ExternalLink href="mailto:joeaquiare@gmail.com">joeaquiare@gmail.com</ExternalLink>.
       </TextBlock>
       <TextBlock>
-        This site is always a work in progress.<br/><br/>
+        This site is always a work in progress.
       </TextBlock>
-      <TextBlock style={{ paddingLeft: "40%" }}>
-      &nbsp;／l、<br/>
-      （ﾟ､ ｡ ７<br/>
-      &nbsp;&nbsp;l  ~ヽ<br/>
-      &nbsp;&nbsp;じしf_,)ノ<br/>
+      <TextBlock style={{ paddingLeft: "40%", userSelect: "none" }}>
+        <br/>
+        &nbsp;／l、<br/>
+        （ﾟ､ ｡ ７<br/>
+        &nbsp;&nbsp;l  ~ヽ<br/>
+        &nbsp;&nbsp;じしf_,)ノ<br/>
       </TextBlock>
     </SitePage>
   );
