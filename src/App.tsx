d60f8c848f89e92f656c4a2d5935eba746f433eb
unknown <joeaquiare@gmail.com>
2026-06-08 15:32:10 -0400

    Updated site navigation.

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import blogEntries from "./pages/blog/BlogEntries";
 import Music from "./pages/Music";
 import Art from "./pages/Art";
 import Credits from "./pages/Credits";
-import Projects from "./pages/Projects";
 
 // Base app element
 function App() {
@@ -25,7 +24,6 @@ function App() {
               <Route path="/home" element={<Home />} />
               <Route path="/contact" element={<Contact />} />
               <Route path="/credits" element={<Credits />} />
-              <Route path="/projects" element={<Projects />} />
               <Route path="/music" element={<Music />} />
               <Route path="/art" element={<Art />} />
               <Route path="/blog" element={<Blog />} />
